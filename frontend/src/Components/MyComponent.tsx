0a7aa970a52fc338632add73840d9e803c224fc4
nannabergquist <bergquistnanna@gmail.com>
2022-05-09 11:54:12 +0200

    changes ok

---
+++ frontend/src/Components/MyComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyComponent() {
+  return (
+    <article>
+        <p>Min komponent</p>
+    </article>
+  )
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyComponent from './Components/MyComponent';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+   <MyComponent />
+  </React.StrictMode>
+);
